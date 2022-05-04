2087da74287f322c0ab8b495caf759f8712f9e9e
vifieh <vifiehruth@gmail.com>
2022-05-04 16:22:36 +0100

    completed crud for roomName

--- src/main/java/com/reservation/reservationservice/controller/RoomNameController.java
+++ src/main/java/com/reservation/reservationservice/controller/RoomNameController.java
@@ -1,9 +1,78 @@
 package com.reservation.reservationservice.controller;
 
+import com.reservation.reservationservice.dto.CustomDto;
+import com.reservation.reservationservice.dto.ResponseMessage;
+import com.reservation.reservationservice.model.RoomName;
+import com.reservation.reservationservice.payload.CustomPayload;
+import com.reservation.reservationservice.service.RoomNameService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("api/v1/protected/")
 @CrossOrigin
 @RestController
 public class RoomNameController {
+
+    @Autowired
+    RoomNameService roomNameService;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    private String message = null;
+
+    @PostMapping("roomNames/roomTypes/{roomTypeId}")
+    public ResponseEntity<ResponseMessage> createRoomName(@PathVariable("roomTypeId") String roomTypeId,
+                                                          @RequestBody @Valid CustomPayload roomNamePayload) {
+        RoomName roomName = convertRoomNamePayloadToRoomName(roomNamePayload);
+        roomNameService.createRoomName(roomTypeId, roomName);
+        message = "Room Name created successfully!";
+       return new ResponseEntity<>(new ResponseMessage(message), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("roomNames/{roomNameId}")
+    public ResponseEntity<ResponseMessage> editRoomName(@PathVariable("roomNameId") String roomNameId,
+                                                          @RequestBody @Valid CustomPayload roomNamePayload) {
+        RoomName roomName = convertRoomNamePayloadToRoomName(roomNamePayload);
+        roomNameService.editRoomName(roomNameId, roomName);
+        message = "Room Name edited successfully!";
+        return new ResponseEntity<>(new ResponseMessage(message), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("roomNames")
+    public ResponseEntity<List<CustomDto>> getRoomNames() {
+        List<RoomName> roomNames = roomNameService.getAllRoomNames();
+        List<CustomDto> roomNameDtos = convertRoomNamesToRoomNameDtos(roomNames);
+        return new ResponseEntity<>(roomNameDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("roomNames/roomTypes/{roomTypeId}")
+    public ResponseEntity<List<CustomDto>> getRoomNamesByRoomType(@PathVariable("roomTypeId") String roomTypeId) {
+        List<RoomName> roomNames = roomNameService.getRoomNamesByRoomType(roomTypeId);
+        List<CustomDto> roomNameDtos = convertRoomNamesToRoomNameDtos(roomNames);
+        return new ResponseEntity<>(roomNameDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("roomNames/{roomNameId}")
+    public ResponseEntity<CustomDto> getRoomName(@PathVariable("roomNameId") String roomNameId) {
+        RoomName roomName = roomNameService.getRoomName(roomNameId);
+        CustomDto roomNameDto = this.modelMapper.map(roomName, CustomDto.class);
+        return new ResponseEntity<>(roomNameDto, HttpStatus.OK);
+    }
+
+    private RoomName convertRoomNamePayloadToRoomName(CustomPayload roomNamePayload) {
+        return this.modelMapper.map(roomNamePayload, RoomName.class);
+    }
+
+    private List<CustomDto> convertRoomNamesToRoomNameDtos(List<RoomName> roomNames) {
+        return roomNames.stream().map(roomName ->
+                this.modelMapper.map(roomName, CustomDto.class)).collect(Collectors.toList());
+    }
 }
